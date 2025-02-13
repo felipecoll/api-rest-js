05c9cf7db80e86794feb60ea9b90fffc6432e973
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-13 15:52:04 -0300

    Estructura html, css y js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-const apiKey = ''
+import { API_KEY } from "./keys"
+
 const url = 'http://api.themoviedb.org/3/trending/movie/day'
 
 async function getTrendingMovies () {
-    const res = await fetch(`${url}?api_key=${apiKey}`)
+    const res = await fetch(`${url}?api_key=${API_KEY}`)
     const data = await res.json()
 
     const movies = data.results;
